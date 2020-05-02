c9fe6361266f3371f8cdf30c72bfa1816e7ab566
Luty <lutydata@gmail.com>
2020-05-02 13:32:55 +0800

    Update AudioManager

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AudioManager : BaseManager<AudioManager>
+{
+    private AudioSource BGM = null;
+    public float BGMVolume = 1;
+    public float soundVolume = 1;
+
+    private GameObject soundObj = null;
+    private List<AudioSource> soundList = new List<AudioSource>();
+
+
+    public AudioManager()
+    {
+        MonoManager.GetInstance().AddUpdateListener(Update);
+    }
+
+    private void Update() {
+        for (int i = soundList.Count - 1; i >= 0 ; --i)
+        {
+            if(!soundList[i].isPlaying)
+            {
+                this.StopSound(soundList[i]);
+            }
+        }
+    }
+    public void PlayBGM(string name)
+    {
+        if(BGM == null)
+        {
+            GameObject obj = new GameObject();
+            obj.name = "BGM";
+            BGM = obj.AddComponent<AudioSource>();
+        }
+
+        ResourceManager.GetInstance().LoadAsync<AudioClip>("Resources/BGM/" + name, (clip) =>
+        {
+            BGM.clip = clip;
+            BGM.loop = true;
+            BGM.volume = BGMVolume;
+            BGM.Play();
+        });
+    }
+
+    public void ChangeBGMVolume(float v)
+    {
+        BGMVolume = v;
+        if(BGM == null)
+            return;
+        BGM.volume = BGMVolume;
+    }
+    public void PauseBGM()
+    {
+        if(BGM == null)
+            return;
+        BGM.Pause();
+    }
+    public void StopBGM()
+    {
+        if(BGM == null)
+            return;
+        BGM.Stop();
+    }
+
+    public void PlaySound(string name, UnityAction<AudioSource> callback)
+    {
+        if(soundObj == null)
+        {
+            soundObj = new GameObject();
+            soundObj.name = "Sound";
+        }
+        ResourceManager.GetInstance().LoadAsync<AudioClip>("Resources/Sound/" + name, (clip)=>
+        {
+            AudioSource source = soundObj.AddComponent<AudioSource>();
+            source.clip = clip;
+            source.volume = soundVolume;
+            source.Play();
+            soundList.Add(source);
+            if(callback!=null)
+                callback(source);
+
+        });
+    }
+
+    public void changeSoundVolume(float v)
+    {
+        soundVolume = v;
+        for(int i =0;i < soundList.Count; ++i )
+            soundList[i].volume = v;
+    }
+    public void StopSound(AudioSource source)
+    {
+        if(soundList.Contains(source))
+        {
+            soundList.Remove(source);
+            source.Stop();
+            GameObject.Destroy(source);
+        }
+    }
+}
