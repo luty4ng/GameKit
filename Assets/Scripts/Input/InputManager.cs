b2f8edbaee73327567c473d50d28e84df01ba605
Luty <18810727622>
2020-10-06 10:37:52 +0800

    Update Input Manager

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -26,13 +26,40 @@ public class InputManager : BaseManager<InputManager>
             EventCenter.GetInstance().EventTrigger("KeyUp", key);
         }
     }
+
+    private void CheckMovement()
+    {
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        EventCenter.GetInstance().EventTrigger<float>("Horizontal", Horizontal);
+        EventCenter.GetInstance().EventTrigger<float>("Vertical", Vertical);
+    }
+
+    private void CheckButtonDown(String name)
+    {
+        if(Input.GetButtonDown(name))
+            EventCenter.GetInstance().EventTrigger(name + " ButtonDown");
+    }
+
+    private void CheckButtonUp(string name)
+    {
+        if(Input.GetButtonUp(name))
+            EventCenter.GetInstance().EventTrigger(name + " ButtonUp");
+    }
+
     private void MyUpdate()
     {
         if(!isActive)
             return;
-        CheckKeyCode(KeyCode.W);
-        CheckKeyCode(KeyCode.A);
-        CheckKeyCode(KeyCode.S);
-        CheckKeyCode(KeyCode.D);
+        CheckKeyCode(KeyCode.Q);
+        CheckKeyCode(KeyCode.E);
+        //CheckKeyCode(KeyCode.S);
+        //CheckKeyCode(KeyCode.D);
+
+        CheckMovement();
+        CheckButtonDown("Jump");
+        CheckButtonDown("Fire1");
+        CheckButtonUp("Fire1");
     }
 }
