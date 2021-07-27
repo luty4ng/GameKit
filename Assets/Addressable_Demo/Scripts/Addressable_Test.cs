7e78bde620fe512ffa2f69bbb2f8aba014ab9129
Luty <lutydata@gmail.com>
2021-07-27 10:12:55 +0800

    features: Update

---
+++ Assets/Addressable_Demo/Scripts/Addressable_Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class Addressable_Test : MonoBehaviour
+{
+    void Start()
+    {
+        Addressables.LoadAssetAsync<Material>("Assets/Addressable_Demo/Prefabs/Capsule.prefab[Assets/Arts/Addressable/Black 1.mat]");
+    }
+
+
+}
