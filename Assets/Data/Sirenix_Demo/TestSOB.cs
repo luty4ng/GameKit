2780493ffa154572d66facc55a801c5e327da86a
Luty <lutydata@gmail.com>
2021-08-03 18:24:18 +0800

    chore: directory update

--- Assets/Data/Sirenix_Demo/TestSOB.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using Sirenix.OdinInspector;
-
-[CreateAssetMenu(fileName = "TestSOB", menuName = "TestSOB")]
-public class TestSOB : SerializedScriptableObject
-{
-    public ItemData[] aaaa;
-}
